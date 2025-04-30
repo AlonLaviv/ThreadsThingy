0a02cf47d5a4a87598e75f630975eb1f26b3188d
Alon Laviv <laviv.alon@gmail.com>
2025-04-30 14:04:40 +0300

    hi2

--- app/src/main/java/com/example/threadsapp/MainActivity.java
+++ app/src/main/java/com/example/threadsapp/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.threadsapp;
 
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,47 +14,39 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button;
-    private TextView textView;
-    private int clickCount = 0;
-    private int seconds = 0;
-    private Handler handler = new Handler();
-    private Runnable timerRunnable = new Runnable() {
-        @Override
-        public void run() {
-            seconds += 2;
-            textView.setText(String.valueOf(seconds));
-            handler.postDelayed(this, 2000);
-        }
-    };
+    Button button;
+    TextView textView;
+    int clickCount = 0;
+    int secondsElapsed = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
         button = findViewById(R.id.button);
         textView = findViewById(R.id.textView);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 clickCount++;
-                Toast.makeText(MainActivity.this, String.valueOf(clickCount), Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Click: " + clickCount, Toast.LENGTH_SHORT).show();
             }
         });
-        handler.postDelayed(timerRunnable, 2000);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        handler.removeCallbacks(timerRunnable);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {Thread.sleep(2000);} catch (InterruptedException hi) {hi.printStackTrace();}
+                    secondsElapsed += 2;
+                    runOnUiThread(new Runnable() { @Override public void run() {textView.setText("Seconds: " + secondsElapsed);}});
+                }
+            }
+        }).start();
     }
 }
